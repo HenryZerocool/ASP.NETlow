a0ed4e4ed81bce3187e3cfc4ac75a6349dd1b9df
HenryZerocool <phnguyen7@myseneca.ca>
2021-04-08 17:40:36 -0400

    basic project configurations

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -10,40 +11,55 @@ using System.Threading.Tasks;
 
 namespace ASP.NETlow
 {
-	public class Startup
-	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
-		{
-		}
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // customer error handler page
+                app.UseExceptionHandler("/error.html");
+            }
+            // user static files
+            app.UseFileServer();
 
-			app.UseFileServer();
+            app.UseRouting();
 
-			app.UseRouting();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapGet("/hello/{name:alpha}", async context =>
-				{
-					var name = context.Request.RouteValues["name"];
-					Console.WriteLine("console change " + name);
-					await context.Response.WriteAsync($"Hello from {name}!");
-				});
-				endpoints.MapGet("/", async context =>
-				{
-					//var name = context.Request.RouteValues["name"];
-					await context.Response.WriteAsync($"Hello World!");
-				});
-			});
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/hello/{name:alpha}", async context =>
+                {
+                    var name = context.Request.RouteValues["name"];
+                    Console.WriteLine("console change " + name);
+                    await context.Response.WriteAsync($"Hello from {name}!");
+                });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"Hello World!");
+                });
+                endpoints.MapGet("/invalid", async context =>
+                {
+                    // await context.Response.WriteAsync($"Hello World!");
+                    throw new Exception("ERROR!!!");
+                });
+            });
+        }
+    }
 }
